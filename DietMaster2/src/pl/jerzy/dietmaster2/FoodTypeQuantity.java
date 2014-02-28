ec591443a535d63f9d414615e0b1aeaf5b7a193d
Jerzy <Jerzy@MyLaptop>
2014-02-28 16:41:58 +0100

    Choosing portions quantity implemented.

---
+++ DietMaster2/src/pl/jerzy/dietmaster2/FoodTypeQuantity.java
@@ -0,0 +1,78 @@
+package pl.jerzy.dietmaster2;
+
+public class FoodTypeQuantity {
+float dairyQuantity;
+float fruitQuantity;
+float grainQuantity;
+float meatQuantity;
+float oilQuantity;
+float sweetQuantity;
+float vegetableQuantity;
+float waterQuantity;
+
+public FoodTypeQuantity()
+{
+	float dairyQuantity=0;
+	float fruitQuantity=0;
+	float grainQuantity=0;
+	float meatQuantity=0;
+	float oilQuantity=0;
+	float sweetQuantity=0;
+	float vegetableQuantity=0;
+	float waterQuantity=0;
+}
+
+public float getDairyQuantity() {
+	return dairyQuantity;
+}
+public void setDairyQuantity(float dairyQuantity) {
+	this.dairyQuantity = dairyQuantity;
+}
+public float getFruitQuantity() {
+	return fruitQuantity;
+}
+public void setFruitQuantity(float fruitQuantity) {
+	this.fruitQuantity = fruitQuantity;
+}
+public float getGrainQuantity() {
+	return grainQuantity;
+}
+public void setGrainQuantity(float grainQuantity) {
+	this.grainQuantity = grainQuantity;
+}
+public float getMeatQuantity() {
+	return meatQuantity;
+}
+public void setMeatQuantity(float meatQuantity) {
+	this.meatQuantity = meatQuantity;
+}
+public float getOilQuantity() {
+	return oilQuantity;
+}
+public void setOilQuantity(float oilQuantity) {
+	this.oilQuantity = oilQuantity;
+}
+public float getSweetQuantity() {
+	return sweetQuantity;
+}
+public void setSweetQuantity(float sweetQuantity) {
+	this.sweetQuantity = sweetQuantity;
+}
+public float getVegetableQuantity() {
+	return vegetableQuantity;
+}
+public void setVegetableQuantity(float vegetableQuantity) {
+	this.vegetableQuantity = vegetableQuantity;
+}
+public float getWaterQuantity() {
+	return waterQuantity;
+}
+public void setWaterQuantity(float waterQuantity) {
+	this.waterQuantity = waterQuantity;
+}
+
+
+
+
+
+}
--- DietMaster2/src/pl/jerzy/dietmaster2/QuantitySetter.java
+++ DietMaster2/src/pl/jerzy/dietmaster2/QuantitySetter.java
@@ -2,83 +2,228 @@ package pl.jerzy.dietmaster2;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
 
-public class QuantitySetter extends Activity implements OnClickListener {
-	Button dairyPlus;
-	Button dairyMinus;
-	Button fruitPlus;
-	Button fruitMinus;
-	Button grainPlus;
-	Button grainMinus;
-	Button meatPlus;
-	Button meatMinus;
-	Button oilPlus;
-	Button oilMinus;
-	Button sweetPlus;
-	Button sweetMinus;
-	Button vegetablePlus;
-	Button vegetableMinus;
-	Button waterPlus;
-	Button waterMinus;
+public class QuantitySetter extends Activity {
+
+	Button plus;
+	Button minus;
+	EditText quantity;
+	FoodTypeQuantity foodQuantity = new FoodTypeQuantity();
+	GridView foodTypeGrid;
+	int selectedItemPosition = 0;
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+		case R.id.next:
+			Intent intent = new Intent(this, MainActivity.class);
+			// intent.putExtra(EXTRA_MESSAGE, message);
+			startActivity(intent);
+			this.finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.to_main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	private void setFoodQuantity(float quantityTemp) {
+		switch (selectedItemPosition) {
+		case 0:
+			foodQuantity.setDairyQuantity(quantityTemp);
+
+			break;
+		case 1:
+			foodQuantity.setFruitQuantity(quantityTemp);
+			break;
+		case 2:
+			foodQuantity.setGrainQuantity(quantityTemp);
+			break;
+		case 3:
+			foodQuantity.setMeatQuantity(quantityTemp);
+			break;
+		case 4:
+			foodQuantity.setOilQuantity(quantityTemp);
+			break;
+		case 5:
+			foodQuantity.setSweetQuantity(quantityTemp);
+			break;
+		case 6:
+			foodQuantity.setVegetableQuantity(quantityTemp);
+			break;
+		case 7:
+			foodQuantity.setWaterQuantity(quantityTemp);
+
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quantity_setter);
-		dairyPlus = (Button) findViewById(R.id.dairyPlus);
-		dairyMinus = (Button) findViewById(R.id.dairyMinus);
-		fruitPlus = (Button) findViewById(R.id.fruitPlus);
-		fruitMinus = (Button) findViewById(R.id.fruitMinus);
-		grainPlus = (Button) findViewById(R.id.grainPlus);
-		grainMinus = (Button) findViewById(R.id.grainMinus);
-		meatPlus = (Button) findViewById(R.id.meatPlus);
-		meatMinus = (Button) findViewById(R.id.meatMinus);
-		oilPlus = (Button) findViewById(R.id.oilPlus);
-		oilMinus = (Button) findViewById(R.id.oilMinus);
-		sweetPlus = (Button) findViewById(R.id.sweetPlus);
-		sweetMinus = (Button) findViewById(R.id.sweetMinus);
-		vegetablePlus = (Button) findViewById(R.id.vegetablePlus);
-		vegetableMinus = (Button) findViewById(R.id.vegetableMinus);
-		waterPlus = (Button) findViewById(R.id.waterPlus);
-		waterMinus = (Button) findViewById(R.id.waterMinus);
-		dairyPlus.setOnClickListener(this);
-		dairyMinus.setOnClickListener(this);
-		fruitPlus.setOnClickListener(this);
-		fruitMinus.setOnClickListener(this);
-		grainPlus.setOnClickListener(this);
-		grainMinus.setOnClickListener(this);
-		meatPlus.setOnClickListener(this);
-		meatMinus.setOnClickListener(this);
-		oilPlus.setOnClickListener(this);
-		oilMinus.setOnClickListener(this);
-		sweetPlus.setOnClickListener(this);
-		sweetMinus.setOnClickListener(this);
-		vegetablePlus.setOnClickListener(this);
-		vegetableMinus.setOnClickListener(this);
-		waterPlus.setOnClickListener(this);
-		waterMinus.setOnClickListener(this);
+		plus = (Button) findViewById(R.id.plus);
+		minus = (Button) findViewById(R.id.minus);
+		foodTypeGrid = (GridView) findViewById(R.id.gridview);
+		quantity = (EditText) findViewById(R.id.quantity);
+		plus.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				float quantityTemp = Float.parseFloat(quantity.getText()
+						.toString());
+				quantityTemp += 0.5f;
+				quantity.setText(String.valueOf(quantityTemp));
+				setFoodQuantity(quantityTemp);
+			}
+		});
+
+		minus.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				float quantityTemp = Float.parseFloat(quantity.getText()
+						.toString());
+				quantityTemp -= 0.5f;
+				quantity.setText(String.valueOf(quantityTemp));
+				setFoodQuantity(quantityTemp);
+			}
+		});
+		GridView gridview = (GridView) findViewById(R.id.gridview);
+		gridview.setAdapter(new ImageAdapter(this));
+
+		gridview.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v,
+					int position, long id) {
+				((Button) findViewById(R.id.plus)).setEnabled(true);
+				((Button) findViewById(R.id.minus)).setEnabled(true);
+				/*
+				 * Toast.makeText(QuantitySetter.this, "" + position,
+				 * Toast.LENGTH_SHORT).show();
+				 */
+				selectedItemPosition = position;
+				switch (position) {
+				case 0:
+					quantity.setText(String.valueOf(foodQuantity
+							.getDairyQuantity()));
+					Toast.makeText(
+							QuantitySetter.this,
+							getResources().getText(R.string.youSelected)
+									.toString()
+									+ " "
+									+ getResources().getText(R.string.dairy)
+											.toString(), Toast.LENGTH_SHORT)
+							.show();
+					break;
+				case 1:
+					quantity.setText(String.valueOf(foodQuantity
+							.getFruitQuantity()));
+					Toast.makeText(
+							QuantitySetter.this,
+							getResources().getText(R.string.youSelected)
+									.toString()
+									+ " "
+									+ getResources().getText(R.string.fruit)
+											.toString(), Toast.LENGTH_SHORT)
+							.show();
+					break;
+				case 2:
+					quantity.setText(String.valueOf(foodQuantity
+							.getGrainQuantity()));
+					Toast.makeText(
+							QuantitySetter.this,
+							getResources().getText(R.string.youSelected)
+									.toString()
+									+ " "
+									+ getResources().getText(R.string.grain)
+											.toString(), Toast.LENGTH_SHORT)
+							.show();
+					break;
+				case 3:
+					quantity.setText(String.valueOf(foodQuantity
+							.getMeatQuantity()));
+					Toast.makeText(
+							QuantitySetter.this,
+							getResources().getText(R.string.youSelected)
+									.toString()
+									+ " "
+									+ getResources().getText(R.string.meat)
+											.toString(), Toast.LENGTH_SHORT)
+							.show();
+					break;
+				case 4:
+					quantity.setText(String.valueOf(foodQuantity
+							.getOilQuantity()));
+					Toast.makeText(
+							QuantitySetter.this,
+							getResources().getText(R.string.youSelected)
+									.toString()
+									+ " "
+									+ getResources().getText(R.string.oil)
+											.toString(), Toast.LENGTH_SHORT)
+							.show();
+					break;
+				case 5:
+					quantity.setText(String.valueOf(foodQuantity
+							.getSweetQuantity()));
+					Toast.makeText(
+							QuantitySetter.this,
+							getResources().getText(R.string.youSelected)
+									.toString()
+									+ " "
+									+ getResources().getText(R.string.sweet)
+											.toString(), Toast.LENGTH_SHORT)
+							.show();
+					break;
+				case 6:
+					quantity.setText(String.valueOf(foodQuantity
+							.getVegetableQuantity()));
+					Toast.makeText(
+							QuantitySetter.this,
+							getResources().getText(R.string.youSelected)
+									.toString()
+									+ " "
+									+ getResources()
+											.getText(R.string.vegetable)
+											.toString(), Toast.LENGTH_SHORT)
+							.show();
+					break;
+				case 7:
+					quantity.setText(String.valueOf(foodQuantity
+							.getWaterQuantity()));
+					Toast.makeText(
+							QuantitySetter.this,
+							getResources().getText(R.string.youSelected)
+									.toString()
+									+ " "
+									+ getResources().getText(R.string.water)
+											.toString(), Toast.LENGTH_SHORT)
+							.show();
+				default:
+					break;
+				}
+
+			}
+		});
+
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.quantity_setter, menu);
-		return true;
-
-	}
-
-	@Override
-	public void onClick(View arg0) {
-if (arg0.equals(oilMinus)) {
-	findViewById(R.id.etOilQuantity).
-} else {
-
-}
-		
-	}
-
 }
